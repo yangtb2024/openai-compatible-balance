6c9c718ff5b061a86a4791aacc5e54598712d9d1
yangtb2024 <164613316+yangtb2024@users.noreply.github.com>
2025-01-06 18:49:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -148,4 +148,4 @@ async def health_check(authorization: str = Header(None)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
